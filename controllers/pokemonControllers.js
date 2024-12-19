a5d79c94b70e3fd76432be4a0ee72464a3556b4a
danielacaicedo03 <daniela.caicedo06@usc.edu.co>
2024-12-18 21:20:44 -0500

    creacion catch pokemon

--- controllers/pokemonControllers.js
+++ controllers/pokemonControllers.js
@@ -2,9 +2,6 @@ const StatusPokemon = require("../models/pokemonModels")
 exports.test = (req,res)=>{
         console.log("hola controller")
         res.status(200).send("Hola desde controller")
-
-        
-
 }
 
 
@@ -35,11 +32,56 @@ const status = await StatusPokemon.create({
         return res.status(500).json({error})
     }
 
-
-
-
-
-
-
-
  }
+exports.getPokemonByPokemonId = async (req,res)=>{
+    try {
+        const pokemon_id = req.params.pokemon_id;
+        let pokemon = await StatusPokemon.findOne({"pokemon_id":pokemon_id});
+        if (!pokemon){
+            res.status(404).json({message:"pokemon not found"})
+        }else {
+            res.status(200).json(pokemon)
+        }
+     
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json({error})
+    }
+}
+
+
+exports.catchPokemonByPokemonId =async (req,res)=>{
+    const pokemon_id = req.params.pokemon_id;
+    const pokemonStatusId = req.body.pokemon_id
+    if(pokemon_id==pokem)
+
+    try {
+        
+        const pokemonStatusView = req.body.view;
+        const pokemonStatusCatch = req.body.catch
+        let pokemon = await StatusPokemon.findOne({"pokemon_id":pokemon_id})
+        if (!pokemon){
+            return res.status(400).json({message:"Bad request, pokemon not view yet"})
+        }
+        else if (pokemon.catch){
+            return res.status(200).json(pokemon)
+         
+        }
+        else{
+            pokemon = await StatusPokemon
+            .findOneAndReplace({"pokemon_id":pokemon_id},{
+                pokemon_id:pokemon_id,
+                view:true,
+                catch:true,
+            },{new:true}
+            )
+            return res.status(200).json(pokemon)
+            }
+
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json({error})
+    }
+
+
+}
